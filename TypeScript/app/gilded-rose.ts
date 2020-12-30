1ae6382a7cd4f9d64fed13594e63f91f95ed5aa9
Richard Kasperowski <r@kasperowski.com>
2020-12-29 22:34:17 -0500

    Start grouping related biz logic together

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -15,10 +15,8 @@ const backstagePasses: string = 'Backstage passes to a TAFKAL80ETC concert';
 const sulfuras: string = 'Sulfuras, Hand of Ragnaros';
 
 const maxQuality: number = 50;
-
-const minQuality = 0;
-
-const minSellIn = 0;
+const minQuality: number = 0;
+const minSellIn: number = 0;
 
 export class GildedRose {
     items: Array<Item>;
@@ -30,46 +28,59 @@ export class GildedRose {
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             let item: Item = this.items[i];
-            if (item.name != agedBrie && item.name != backstagePasses) {
-                if (item.quality > minQuality) {
-                    if (item.name != sulfuras) {
-                        item.quality--
-                    }
+
+            if (item.name == agedBrie) {
+                item.sellIn--;
+                if (item.sellIn < minSellIn) {
+                    item.quality += 2;
+                } else {
+                    item.quality++;
+                }
+                if (item.quality > maxQuality) {
+                    item.quality = maxQuality;
                 }
             } else {
-                if (item.quality < maxQuality) {
-                    item.quality++
-                    if (item.name == backstagePasses) {
-                        if (this.concertHappeningSoon(item)) {
-                            if (item.quality < maxQuality) {
-                                item.quality++
-                            }
+                if (item.name != agedBrie && item.name != backstagePasses) {
+                    if (item.quality > minQuality) {
+                        if (item.name != sulfuras) {
+                            item.quality--
                         }
-                        if (this.concertHappeningEvenSooner(item)) {
-                            if (item.quality < maxQuality) {
-                                item.quality++
-                            }
-                        }
-                    }
-                }
-            }
-            if (item.name != sulfuras) {
-                item.sellIn--;
-            }
-            if (item.sellIn < minSellIn) {
-                if (item.name != agedBrie) {
-                    if (item.name != backstagePasses) {
-                        if (item.quality > minQuality) {
-                            if (item.name != sulfuras) {
-                                item.quality--
-                            }
-                        }
-                    } else {
-                        item.quality = minQuality
                     }
                 } else {
                     if (item.quality < maxQuality) {
                         item.quality++
+                        if (item.name == backstagePasses) {
+                            if (this.concertHappeningSoon(item)) {
+                                if (item.quality < maxQuality) {
+                                    item.quality++
+                                }
+                            }
+                            if (this.concertHappeningEvenSooner(item)) {
+                                if (item.quality < maxQuality) {
+                                    item.quality++
+                                }
+                            }
+                        }
+                    }
+                }
+                if (item.name != sulfuras) {
+                    item.sellIn--;
+                }
+                if (item.sellIn < minSellIn) {
+                    if (item.name != agedBrie) {
+                        if (item.name != backstagePasses) {
+                            if (item.quality > minQuality) {
+                                if (item.name != sulfuras) {
+                                    item.quality--
+                                }
+                            }
+                        } else {
+                            item.quality = minQuality
+                        }
+                    } else {
+                        if (item.quality < maxQuality) {
+                            item.quality++
+                        }
                     }
                 }
             }
